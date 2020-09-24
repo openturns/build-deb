4816dc0913d56b43343d683c9429662c1b4fe2c9
Julien Schueller <schueller@phimeca.com>
2020-09-24 16:36:56 +0200

    debian

--- run_docker_build.sh
+++ run_docker_build.sh
@@ -15,16 +15,14 @@ VERSION=$1
 parent_dir=`readlink -f $0`
 parent_dir=`dirname $parent_dir`
 
-
+triplets="ubuntu:xenial:xUbuntu_16.04 ubuntu:bionic:xUbuntu_18.04 ubuntu:focal:xUbuntu_20.04 debian:stretch:Debian_9.0 debian:buster:Debian_10"
 cd /tmp
-for triplet in ubuntu:xenial:xUbuntu_16.04 ubuntu:bionic:xUbuntu_18.04
-#ubuntu:bionic:xUbuntu_18.04 ubuntu:focal:xUbuntu_20.04
+for triplet in ${triplets}
 do
   distro=`echo "${triplet}" | cut -d ":" -f 1`
   code=`echo "${triplet}" | cut -d ":" -f 2`
   tag=`echo "${triplet}" | cut -d ":" -f 3`
-  wget --no-check-certificate --recursive --no-parent -A.deb -P input/${distro}/${code} --no-directories https://download.opensuse.org/repositories/science:/openturns/${tag}/amd64/
-#   wget --no-check-certificate https://download.opensuse.org/repositories/science:/openturns/${tag}/amd64/libopenturns0.16_1.15-1_amd64.deb -P input/${distro}/${code}
+  wget --no-check-certificate --recursive --no-parent -A.deb -R "*dbgsym*" -P input/${distro}/${code} --no-directories https://download.opensuse.org/repositories/science:/openturns/${tag}/amd64/
   tree input/${distro}/${code}/
   for debfile in `find input/${distro}/${code}/ -name "*.deb"`
   do
@@ -36,7 +34,7 @@ cp -rv /io/debian /io/ubuntu .
 gpg --import /io/private.key
 gpg --list-keys
 cd /tmp
-for triplet in ubuntu:xenial:xUbuntu_16.04 ubuntu:bionic:xUbuntu_18.04
+for triplet in ${triplets}
 do
   distro=`echo "${triplet}" | cut -d ":" -f 1`
   code=`echo "${triplet}" | cut -d ":" -f 2`
@@ -45,14 +43,13 @@ do
   do
     reprepro --basedir ${distro} includedeb ${code} ${debfile}
   done
-  tree ${distro}
 done
 
 uid=$2
 gid=$3
 if test -n "${uid}" -a -n "${gid}"
 then
-  for distro in ubuntu
+  for distro in ubuntu debian
   do
     cp -rv ${distro}/* /io/${distro}
     chown ${uid}:${gid} /io/${distro}

2fd5b7ec656df86080d16d4a21143e39a38e60b4
Julien Schueller <schueller@phimeca.com>
2024-01-09 17:09:55 +0100

    Update for 1.22

--- convert-deb.sh
+++ convert-deb.sh
@@ -31,7 +31,7 @@ dpkg -e $DEB
 dpkg -x $DEB .
 
 # modify ot version
-OT_VERSION="1.22~rc1-1"
+OT_VERSION="1.22-1"
 OT_MODVERSION="$OT_VERSION$code"
 sed -i -e "s/$OT_VERSION/$OT_MODVERSION/" DEBIAN/control
 
@@ -52,6 +52,13 @@ if grep -q "pagmo" <<< "$DEB"
 then
   PAGMO_VERSION="2.19.0-1"
   sed -i -e "s/= ${PAGMO_VERSION}/= ${PAGMO_VERSION}${code}/" DEBIAN/control
+fi
+
+# modify pagmo control
+if grep -q "mixmod" <<< "$DEB"
+then
+  MIXMOD_VERSION="2.1.10-1"
+  sed -i -e "s/= ${MIXMOD_VERSION}/= ${MIXMOD_VERSION}${code}/" DEBIAN/control
 fi
 
 cat DEBIAN/control
